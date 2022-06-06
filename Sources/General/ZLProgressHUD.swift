fdb388f4bcedd814e962b33b546c7f5aac2aa77d
longitachi <longitachi@163.com>
2022-06-06 18:10:36 +0800

    Add loading image

--- Sources/General/ZLProgressHUD.swift
+++ Sources/General/ZLProgressHUD.swift
@@ -46,6 +46,15 @@ public class ZLProgressHUD: UIView {
             }
         }
         
+        var icon: UIImage? {
+            switch self {
+            case .light, .lightBlur:
+                return getImage("zl_loading_dark")
+            case .dark, .darkBlur:
+                return getImage("zl_loading_light")
+            }
+        }
+        
         var textColor: UIColor {
             switch self {
             case .light, .lightBlur:
@@ -69,7 +78,7 @@ public class ZLProgressHUD: UIView {
     
     private let style: ZLProgressHUD.HUDStyle
     
-    private lazy var loadingView = ZLLoadingView(frame: CGRect(x: 135 / 2 - 22, y: 25, width: 44, height: 44), style: style)
+    private lazy var loadingView = UIImageView(image: style.icon)
     
     private var timer: Timer?
     
@@ -106,6 +115,7 @@ public class ZLProgressHUD: UIView {
             view.addSubview(effectView)
         }
         
+        loadingView.frame = CGRect(x: 135 / 2 - 22, y: 25, width: 44, height: 44)
         view.addSubview(loadingView)
         
         let label = UILabel(frame: CGRect(x: 0, y: 85, width: view.bounds.width, height: 30))
@@ -118,9 +128,20 @@ public class ZLProgressHUD: UIView {
         addSubview(view)
     }
     
+    private func startAnimation() {
+        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+        animation.fromValue = 0
+        animation.toValue = CGFloat.pi * 2
+        animation.duration = 0.8
+        animation.repeatCount = .infinity
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        loadingView.layer.add(animation, forKey: nil)
+    }
+    
     @objc public func show(timeout: TimeInterval = 100) {
         ZLMainAsync {
-            self.loadingView.startLoading()
+            self.startAnimation()
             UIApplication.shared.keyWindow?.addSubview(self)
         }
         if timeout > 0 {
@@ -133,7 +154,7 @@ public class ZLProgressHUD: UIView {
     @objc public func hide() {
         cleanTimer()
         ZLMainAsync {
-            self.loadingView.stopLading()
+            self.loadingView.layer.removeAllAnimations()
             self.removeFromSuperview()
         }
     }
@@ -146,102 +167,5 @@ public class ZLProgressHUD: UIView {
     func cleanTimer() {
         timer?.invalidate()
         timer = nil
-    }
-}
-
-class ZLLoadingView: UIView {
-    private let style: ZLProgressHUD.HUDStyle
-    
-    private lazy var mainLayer = CAShapeLayer()
-
-    deinit {
-        zl_debugPrint("ZLLoadingView deinit")
-    }
-    
-    init(frame: CGRect, style: ZLProgressHUD.HUDStyle) {
-        self.style = style
-        super.init(frame: frame)
-        
-        setupUI()
-    }
-    
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupUI() {
-        mainLayer.frame = bounds
-        layer.addSublayer(mainLayer)
-        
-        let topColors: [CGColor]
-        let bottomColors: [CGColor]
-        
-        switch style {
-        case .light, .lightBlur:
-            topColors = [
-                UIColor.black.withAlphaComponent(0.5).cgColor,
-                UIColor.black.withAlphaComponent(0.8).cgColor,
-            ]
-            bottomColors = [
-                UIColor.black.withAlphaComponent(0.55).cgColor,
-                UIColor.black.withAlphaComponent(0.05).cgColor,
-            ]
-        case .dark, .darkBlur:
-            topColors = [
-                UIColor.white.withAlphaComponent(0.5).cgColor,
-                UIColor.white.withAlphaComponent(0.8).cgColor,
-            ]
-            bottomColors = [
-                UIColor.white.withAlphaComponent(0.55).cgColor,
-                UIColor.white.withAlphaComponent(0.05).cgColor,
-            ]
-        }
-        
-        let topLayer = CAGradientLayer()
-        topLayer.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height / 2)
-        topLayer.colors = topColors
-        topLayer.startPoint = CGPoint(x: 0, y: 0.5)
-        topLayer.endPoint = CGPoint(x: 1, y: 0.5)
-        mainLayer.addSublayer(topLayer)
-        
-        let bottomLayer = CAGradientLayer()
-        bottomLayer.frame = CGRect(x: 0, y: bounds.height / 2, width: bounds.width, height: bounds.height / 2)
-        bottomLayer.colors = bottomColors
-        bottomLayer.startPoint = CGPoint(x: 0, y: 0.5)
-        bottomLayer.endPoint = CGPoint(x: 1, y: 0.5)
-        mainLayer.addSublayer(bottomLayer)
-        
-        // 创建一个圆形layer
-        let maskLayer = CAShapeLayer()
-        maskLayer.frame = bounds
-        maskLayer.path = UIBezierPath(
-            arcCenter: CGPoint(x: bounds.width / 2, y: bounds.height / 2),
-            radius: bounds.width / 2 - 3.5,
-            startAngle: .pi / 15, endAngle: .pi * 2,
-            clockwise: true
-        ).cgPath
-        maskLayer.lineWidth = 3.5
-        maskLayer.lineCap = .round
-        maskLayer.lineJoin = .round
-        maskLayer.strokeColor = UIColor.black.cgColor
-        maskLayer.fillColor = UIColor.clear.cgColor
-        maskLayer.strokeEnd = 0.95
-        mainLayer.mask = maskLayer
-    }
-    
-    func startLoading() {
-        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
-        animation.fromValue = 0
-        animation.toValue = CGFloat.pi * 2
-        animation.duration = 0.8
-        animation.repeatCount = .infinity
-        animation.fillMode = .forwards
-        animation.isRemovedOnCompletion = false
-        layer.add(animation, forKey: nil)
-    }
-    
-    func stopLading() {
-        layer.removeAllAnimations()
     }
 }
