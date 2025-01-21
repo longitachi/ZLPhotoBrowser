91c6737479477d982469227d134441c8b25a3381
longitachi <longitachi@163.com>
2025-01-21 11:38:20 +0800

    fix #969

--- Sources/General/ZLPhotoPreviewSheet.swift
+++ Sources/General/ZLPhotoPreviewSheet.swift
@@ -334,7 +334,8 @@ public class ZLPhotoPreviewSheet: UIView {
             PHPhotoLibrary.requestAuthorization { status in
                 ZLMainAsync {
                     if status == .denied {
-                        self.showNoAuthorityAlert()
+                        // 不符合苹果审核，这里注释掉 https://github.com/longitachi/ZLPhotoBrowser/issues/969#issuecomment-2601632232
+//                        self.showNoAuthorityAlert()
                     } else if status == .authorized {
                         if self.preview {
                             self.loadPhotos()
